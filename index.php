0f1a517987414c10290ce031fa0143926c5193a6
Thies Mueller <github@td00.de>
2022-06-09 22:53:09 +0200

    php is hard! :<

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 if(isset($_GET['lang'])) { //checks if "?lang=" is set in the url
     $site_lang = $_GET['lang']; //setting the language
 } else { //if there isn't anything in the header, just die already!
-    die(No language selected. abort!); 
+    die("<pre>No language selected. abort!</pre>"); 
 }
 if(isset($_GET['type'])) { //checks if "?type=" is set in the url
     $site_type = $_GET['type']; //setting the type
 } else { //if there isn't anything in the header, just die already!
-    die(No type selected. abort!);
+    die("<pre>No type selected. abort!</pre>");
 }
 if(isset($_GET['page'])) { //checks if "?page=" is set in the url
     $site_name = $_GET['page']; //setting the page
 } else { //if there isn't anything in the header, just die already!
-    die(No page selected. abort!);
+    die("<pre>No page selected. abort!</pre>");
 }
 $site_title = $site_name + " - " + $site_type + " - " + $site_lang; //setting the title;
 $site_url = $site_name + "/" + $site_type + "/" + $site_lang + ".html"; //setting the url;
